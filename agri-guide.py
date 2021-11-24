ae2a811c9230599660cc7f5ae6db6bf822b45fea
Pavithra S <pavi2468kuk@gmail.com>
2021-11-24 18:43:58 +0530

    Update agri-guide.py

--- agri-guide.py
+++ agri-guide.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+import datetime
 from datetime import date, timedelta
 import time
 import sklearn
